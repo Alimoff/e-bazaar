66c85de3c91277a2346f33c93e045ff8bf64ee0f
Alimoff <sobiriddin@gmail.com>
2024-01-29 01:44:16 +0500

    added: checkAuthorized function

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -4,6 +4,7 @@ import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 import bcrypt from 'bcrypt';
 import { UserModel } from '../database/models/user';
 import dotenv from 'dotenv';
+import { Request, Response } from 'express';
 import * as jwt from "jsonwebtoken";
 dotenv.config();
 
@@ -27,6 +28,22 @@ export async function verifyToken(req:any, res:any, next:any) {
     }) 
 }
 
+
+export async function checkAuthorized(req:Request, res: Response){
+    //To get accessToken if user is authorized
+    const authorizationHeader = req.headers.authorization;
+
+    if (!authorizationHeader) {
+        // Handle case where no access token is present
+        return res.status(401).json({ error: 'Unauthorized - Missing token' });}
+
+    const token = authorizationHeader.split(' ')[1];
+      //Get user._id from accessToken
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!) as { id: string };
+    const authorizedUser = decodedToken.id;
+
+    return authorizedUser;
+}
 
 
 
