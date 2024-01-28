abd73fe73c95bc26a5e0fc42b414cf5cb914f20d
Alimoff <sobiriddin@gmail.com>
2024-01-29 03:14:05 +0500

    check validations in each route

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -2,9 +2,15 @@ import { Router } from "express";
 import { ProductController } from "../controller/productController";
 import {verifyToken} from "../config/auth";
 import authenticate from "../middlewares/authenticate"
+import { RequireUserTypes,validate, validateIdParam } from "../middlewares/validate";
+import { createProductValidation, updateProductValidation } from "../validations/schemas/product";
 
 export const productRouter = Router();
 const controller = new ProductController();
+const userType = new RequireUserTypes();
 
-
-productRouter.post("/product", controller.create);
+productRouter.get("/product", controller.getAll);
+productRouter.get("/produc/:id", validateIdParam,controller.get);
+productRouter.post("/product",userType.requireSeller, validate(createProductValidation),controller.create);
+productRouter.put("/product/:id",userType.requireSeller, validate(updateProductValidation),controller.update);
+productRouter.delete("/product/:id",validateIdParam,userType.requireAdmin, userType.requireSeller, controller.delete);
