6f608de0b5ee2a3e1bb7f69c79b981f690ed67a5
Alimoff <sobiriddin@gmail.com>
2024-01-29 03:13:26 +0500

    add new CRUD functions

--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -45,4 +45,94 @@ export class ProductController {
             return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Internal server error" });
         }
     }
+
+    //METHOD PUT
+    //Update specific product by their ID
+    public async update(req: Request, res: Response, next: NextFunction){
+        try{
+            //Function to check if user is authorized
+            checkAuthorized(req,res);
+            
+            const id = req.params;
+            const { product, price, amount,image, category, description } = req.body;
+
+            const owner = checkAuthorized(req, res);
+
+            const sameUser = await ProductModel.findOne({owner});
+            const isProductExist = await ProductModel.findById({_id: id});
+
+            if(isProductExist && sameUser){
+            const updatedProduct = await ProductModel.findOneAndUpdate({_id: id}, {
+                product, price, amount, image, category, description
+            });
+
+            await updatedProduct?.save();
+
+            return res.status(StatusCodes.OK).json({message: updatedProduct});
+            }else{
+                return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: `Product ${product} did not found or only owner ${owner} can update the product!` });
+            }
+        }
+        catch(error){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Internal server error" });
+        }
+    }
+
+    //METHOD DELETE
+    //To delete specific product by their ID
+    public async delete(req: Request, res: Response, next: NextFunction){
+        try{
+              //Function to check if user is authorized
+              checkAuthorized(req,res);
+
+              const id = req.params;
+              const user = checkAuthorized(req, res);
+
+              const findProduct= await ProductModel.findById({_id: id})
+              const checkUser:any =  findProduct?.owner;
+
+              if( checkUser == user ){
+                const deletedProduct = await ProductModel.findByIdAndDelete({_id: id});
+                await deletedProduct?.save();
+
+                return res.status(StatusCodes.OK).json({message: `Product ${findProduct?.product} dfeleted successfully!`});
+              }else{
+                return res.status(StatusCodes.NOT_FOUND).json({message: `Product ${findProduct?.product} not found!`});
+              }
+
+        }catch(error){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Internal server error" });
+        }
+    }
+
+    //METHOD GET - To get all products exist!
+    public async getAll(req:Request, res: Response, next: NextFunction){
+        try{
+            //Function to check if user is authorized
+            checkAuthorized(req,res);
+
+            const products = await ProductModel.find();
+
+            return res.status(StatusCodes.OK).json({mdssage: products});
+
+        }catch(error){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Internal server error" });
+        }
+    }
+
+    //METHOD GET - To get a product by their ID
+    public async get(req:Request, res: Response, next: NextFunction){
+        try{
+            //Function to check if user is authorized
+            checkAuthorized(req,res);
+
+            const id = req.params._id;
+            const product = await ProductModel.findById({_id: id});
+
+            return res.status(StatusCodes.OK).json({mdssage: product});
+
+        }catch(error){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Internal server error" });
+        }
+    }
 }
